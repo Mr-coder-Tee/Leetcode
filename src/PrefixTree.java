c17fc6677ab55dcb42236705d6c54fa4b1225a6d
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-10-15 04:16:19 +0200

    commit

---
+++ src/PrefixTree.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrefixTree {
+
+	public static void main(String[] args) {
+		// https://leetcode.com/problems/implement-trie-prefix-tree/
+
+	}
+	
+}
+class Trie {
+
+    public Trie() {
+        
+    }
+    
+    public void insert(String word) {
+        
+    }
+    
+    public boolean search(String word) {
+    	return true;
+        
+    }
+    
+    public boolean startsWith(String prefix) {
+    	return true;
+    }
+}
+
+  
+  
+ 
